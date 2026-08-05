3759ac78f8dceb4dbdd6131d575d445c4c09a16e
Juan Ignacio Rios <juanignaciorios2003@gmail.com>
2026-08-05 23:33:00 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://apps.apple.com/us/app/offline/id6753837153',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
